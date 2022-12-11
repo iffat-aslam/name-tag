011d4275cb8e661d83517a717e108ed40d7ea52c
Iffat Aslam <Iffata94@gmail.com>
2022-12-10 18:36:13 -0600

    added code for nametag

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-// grab DOM elements
+const nameDisplay = document.getElementById('name-display');
+console.log (nameDisplay);
+nameDisplay.textContent = 'Raven';
+const nameInput = document.getElementById('name-input')
+nameDisplay.style.Color = 'red';
 
-// set event listeners
+const booger = document.getElementById('nametag');
+booger.style.backgroundColor = 'purple';
+
+const button = document.getElementById('update-button');
+button.addEventListener('click',() => {
+    console.log('i am clicking the button');
+    nameDisplay.textContent = nameInput.value;
+});
+
+const pronoun = document.getElementById('update-pronoun');
+button.addEventListener('click',() => {
+    console.log('change');
+    console.log(pronounDisplay)
+    pronounDisplay.textContent = pronounInput.value;
+}
